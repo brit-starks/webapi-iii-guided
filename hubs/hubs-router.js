0a621fe80ab760a173615b0b9532995b6b9f0e8a
Sean Kirkby <sean.kirkby@niceincontact.com>
2019-08-22 03:59:37 -0600

    everybody was kung-fu fighting...

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -5,6 +5,14 @@ const Messages = require('../messages/messages-model.js');
 
 const router = express.Router();
 
+//
+// this is a basic middleware method that just calls next()
+//
+router.use((req, res, next) => {
+  console.log('hubs router baby!');
+  next();
+})
+
 // this only runs if the url has /api/hubs in it
 router.get('/', async (req, res) => {
   try {
@@ -19,27 +27,42 @@ router.get('/', async (req, res) => {
   }
 });
 
-// /api/hubs/:id
+//
+// because this router is bound (in server.js) to
+// the /api/hubs root url, the effective METHOD/url
+// that will trigger this middleware is /api/hubs/:id.
+//
+// note that this method not only supplies the
+// typical arrow function as middleware for the METHOD/url,
+// but it also specifies *another* middleware method (this time
+// by just supplying its name, validateId), as a parameter to
+// .get().
+//
+// validateId() looks up the hub with the id, and if it
+// finds it, it adds the hub object to the req object.
+//
+// That way, we don't have to do another lookup here...
+// we can just use the hub object on req.
+//
+router.get('/:id', validateId, async (req, res) => {
 
-router.get('/:id', async (req, res) => {
-  try {
-    const hub = await Hubs.findById(req.params.id);
+  res.status(200).json(req.hub);
 
-    if (hub) {
-      res.status(200).json(hub);
-    } else {
-      res.status(404).json({ message: 'Hub not found' });
-    }
-  } catch (error) {
-    // log error to server
-    console.log(error);
-    res.status(500).json({
-      message: 'Error retrieving the hub',
-    });
-  }
 });
 
-router.post('/', async (req, res) => {
+//
+// like the GET middleware handler above, this .post()
+// call specifies additional middleware (in addition to 
+// the arrow function): requireBody.
+//
+// the requireBody() middleware just confirms that a body
+// was supplied, and indicates an error condition by calling
+// next(error) if not.
+//
+// that way, we don't have to do the check here, and we can
+// leverage the check elsewhere (like PUT request handlers).
+//
+router.post('/', requireBody, async (req, res) => {
   try {
     const hub = await Hubs.add(req.body);
     res.status(201).json(hub);
@@ -52,7 +75,10 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
+//
+// DELETE requests need an id, so we need to add 
+// validateId to the chain for delete requests.
+router.delete('/:id', validateId, async (req, res) => {
   try {
     const count = await Hubs.remove(req.params.id);
     if (count > 0) {
@@ -69,7 +95,11 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+//
+// the PUT request needs a valid ID *and* a body, so we can
+// add both validateId *and* requireBody to the middleware chain.
+//
+router.put('/:id', validateId, requireBody, async (req, res) => {
   try {
     const hub = await Hubs.update(req.params.id, req.body);
     if (hub) {
@@ -88,7 +118,7 @@ router.put('/:id', async (req, res) => {
 
 // add an endpoint that returns all the messages for a hub
 // this is a sub-route or sub-resource
-router.get('/:id/messages', async (req, res) => {
+router.get('/:id/messages', validateId, async (req, res) => {
   try {
     const messages = await Hubs.findHubMessages(req.params.id);
 
@@ -102,8 +132,16 @@ router.get('/:id/messages', async (req, res) => {
   }
 });
 
+//
+// here is an example of a POST handler that needs both a valid
+// id *and* a message body.
+//
 // add an endpoint for adding new message to a hub
-router.post('/:id/messages', async (req, res) => {
+//
+// note the use of the "rest" operator/syntax "..." (again, not to be
+// confused with REST API's - totally unrelated. Google-fu it.)
+//
+router.post('/:id/messages', validateId, requireBody, async (req, res) => {
   const messageInfo = { ...req.body, hub_id: req.params.id };
 
   try {
@@ -117,5 +155,47 @@ router.post('/:id/messages', async (req, res) => {
     });
   }
 });
+
+//
+// this is our validate() middleware.
+// note that this middleware is not required for *all* 
+// HTTP requests - only those that have an :id parameter.
+//
+// You can add middleware to the chain by calling a .METHOD 
+// method on server() or Router(), or you can add it to the 
+// chain by adding it to the list of methods to be run when
+// a specific METHOD with a specific url is called (as seen above)
+//
+async function validateId(req, res, next) {
+  try {
+    const {id} = req.params;
+
+    const hub = await Hubs.findById(id);
+  
+    if (hub) {
+      req.hub = hub;
+      next();
+    } else {
+      next({message: 'the id you sent was invalid'});
+    }
+  } catch (error) {
+    next({message:'some major jam...'});
+  }
+  
+}
+
+//
+// this middleware is just like validateId. See above.
+//
+function requireBody(req, res, next) {
+  if (req.body && Object.keys(req.body).length > 0) {
+    next();
+  } else {
+    next({message: 'You need to supply a body with this request'});
+  }
+}
+
+
+
 
 module.exports = router;
